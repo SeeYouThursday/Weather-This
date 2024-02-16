08a96f184a1cd36ddfdc9c950cb00424bf83a30c
Brian <galyenb@vcu.edu>
2024-02-16 15:23:00 -0500

    set up User model with email(regex)/password(bcrypt-virtual/method) checks on backend

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+// email String
+// password String
+// savedCities [String]
+
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema = ({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true, 
+        match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    },
+    password: {
+        type: String, 
+        required: true,
+    },
+    savedCities: {
+        type: String,
+        trim: true
+    },
+}, {
+    toJSON: {
+        virtuals: true,
+    }
+})
+
+userSchema.pre('save', async function(next) {
+    if(this.isNew || this.isModified('password')) {
+        const saltRounds = 10
+        this.password = await bcrypt.hash(this.password, saltRounds)
+    }
+
+    next()
+}
+)
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password)
+}
+
+const User = model('User', userSchema);
+
+export default User;
