258a1fadede163ae622934649a6d47de1b9ec672
Brian <galyenb@vcu.edu>
2024-02-20 20:32:47 -0500

    added in Router using v6 of react-router-dom syntax in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // Component Imports
 import App from './App.jsx';
 import LandingPage from './pages/LandingPage.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
 import Dashboard from './pages/Dashboard.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
 // Style Imports
 // import './assets/css/index.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: App,
-  },
-  {
-    path: '*',
-    Component: LandingPage,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     Component: App,
+//   },
+//   {
+//     path: '*',
+//     Component: LandingPage,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/logout" component={Logout} /> //! Will work on redirect to landing on logout*/}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
---
+++ client/src/pages/ErrorPage.jsx
@@ -0,0 +1,8 @@
+export default function ErrorPage() {
+  return (
+    <>
+      <h2>OH DEAR, WE ARE IN TROUBLE</h2>
+      {/* INSERT FILCH MEME */}
+    </>
+  );
+}
