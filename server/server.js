4bcd311b06d59d782cbcee48a0c981508245abbd
Brian <galyenb@vcu.edu>
2024-02-27 14:41:47 -0500

    fixed where app was called before initialization in server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
