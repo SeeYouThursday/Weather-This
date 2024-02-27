7bae87aade766fec64b927bf1eff842d4aba3ea1
Brian <galyenb@vcu.edu>
2024-02-27 15:23:03 -0500

    fixing path to be used in es6 modules

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+// https://www.apollographql.com/docs/apollo-server/migration/#migrate-from-apollo-server-express
 import { fileURLToPath } from 'url';
 import path from 'path';
 import express from 'express';
@@ -10,6 +11,8 @@ import typeDefs from './schema/typeDefs.js';
 import resolvers from './schema/resolvers.js';
 
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const httpServer = http.createServer(app);
