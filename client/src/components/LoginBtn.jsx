87be097a0faf84d7bc4b4a6c23de90101435dae6
Brian <galyenb@vcu.edu>
2024-02-28 13:14:51 -0500

    added authconfig file with domain, clientid, and audience

--- client/src/components/LoginBtn.jsx
+++ client/src/components/LoginBtn.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@material-tailwind/react';
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect } = useAuth0();
 
-  return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+    return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
 };
 
 export default LoginButton;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,12 @@ import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Redirect,
+} from 'react-router-dom';
 // Component Imports
 import App from './App.jsx'; // Uncommented the import statement for the App component
 import LandingPage from './pages/LandingPage.jsx';
@@ -14,28 +19,33 @@ import Results from './pages/Results.jsx';
 // Style Imports
 import './assets/css/index.css';
 
+const auth0Domain = import.meta.env.VITE_DOMAIN; // replace with your Auth0 domain
+const clientId = import.meta.env.VITE_CLIENT_ID; // replace with your Auth0 client ID
+const redirectUri = window.location.origin; // replace with your redirect URI
+
+const url = `https://${auth0Domain}/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=openid%20profile%20email`;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_DOMAIN}
-      clientId={import.meta.env.VITE_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<LandingPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/results" element={<Results />} />
-            {/* <Route path="/logout" component={Logout} /> //! Will work on redirect to landing on logout*/}
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Router>
-    </Auth0Provider>
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-d4jiv6bsx28qa518.us.auth0.com"
+    clientId="mk0DJQkDHbdwRBAaazq8quR5p6w7FZeT"
+    authorizationParams={{
+      redirect_uri: redirectUri,
+    }}
+  >
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/authorize" redirectUri={<Redirect />} /> */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/results" element={<Results />} />
+          {/* <Route path="/logout" component={Logout} /> //! Will work on redirect to landing on logout*/}
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  </Auth0Provider>
 );
