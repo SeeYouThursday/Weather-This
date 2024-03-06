12eda862b580ffa3122d8a5f8c0ed37c71be5495
Brian <galyenb@vcu.edu>
2024-03-06 12:53:32 -0500

    added Suspense to main to handle loading time message

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -14,22 +14,24 @@ const redirectUri = window.location.origin; // replace with your redirect URI
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Auth0Provider
-      domain="dev-d4jiv6bsx28qa518.us.auth0.com"
-      clientId="mk0DJQkDHbdwRBAaazq8quR5p6w7FZeT"
-      authorizationParams={{
-        redirect_uri: redirectUri,
-      }}
-    >
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<LandingPage />} />s
-            {/* <Route path="/logout" component={Logout} /> //! Will work on redirect to landing on logout*/}
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Router>
-    </Auth0Provider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Auth0Provider
+        domain="dev-d4jiv6bsx28qa518.us.auth0.com"
+        clientId="mk0DJQkDHbdwRBAaazq8quR5p6w7FZeT"
+        authorizationParams={{
+          redirect_uri: redirectUri,
+        }}
+      >
+        <Router>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<LandingPage />} />s
+              {/* <Route path="/logout" component={Logout} /> //! Will work on redirect to landing on logout*/}
+            </Route>
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </Router>
+      </Auth0Provider>
+    </Suspense>
   </StrictMode>
 );
